50b39fb97ee6fed79da2b8dba4d7adc79a75bc9a
janeskil1525 <janeskil1525@gmail.com>
2024-12-15 16:34:58 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+Daje::Workflow::Database - It's new $module
+
+# SYNOPSIS
+
+    use Daje::Workflow::Database;
+
+# DESCRIPTION
+
+Daje::Workflow::Database is ...
+
+# LICENSE
+
+Copyright (C) janeskil1525.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+janeskil1525 <janeskil1525@gmail.com>
---
+++ lib/Daje/Workflow/Database.pm
@@ -0,0 +1,39 @@
+package Daje::Workflow::Database;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Daje::Workflow::Database - It's new $module
+
+=head1 SYNOPSIS
+
+    use Daje::Workflow::Database;
+
+=head1 DESCRIPTION
+
+Daje::Workflow::Database is ...
+
+=head1 LICENSE
+
+Copyright (C) janeskil1525.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+janeskil1525 E<lt>janeskil1525@gmail.comE<gt>
+
+=cut
+
