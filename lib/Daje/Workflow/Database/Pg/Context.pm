69eddc57fd4875f22fb5d84fb21bea66a6c9eed1
janeskil1525 <janeskil1525@gmail.com>
2024-12-18 17:50:56 +0100

    First commit

---
+++ lib/Daje/Workflow/Database/Pg/Context.pm
@@ -0,0 +1,22 @@
+package Daje::Workflow::Database::Pg::Context;
+use Mojo::Base -base, -signatures;
+
+has 'db';
+
+sub load($self, $workflow_pkey) {
+
+}
+
+sub save($self, $data) {
+
+}
+
+sub _insert($self, $data) {
+
+}
+
+sub _create($self, $data) {
+
+}
+
+1;
---
+++ lib/Daje/Workflow/Database/Pg/Workflow.pm
@@ -0,0 +1,40 @@
+package Daje::Workflow::Database::Pg::Workflow;
+use Mojo::Base -base, -signatures;
+
+has 'db';
+
+sub load($self, $workflow_pkey) {
+    my $data = $self->db->select(
+        'workflow',
+        {
+            workflow_pkey => $workflow_pkey
+        }
+    );
+}
+
+sub save($self, $data) {
+    if ($data->{workflow_pkey} > 0) {
+        $self->db->update(
+            'workflow',
+            {
+                $data
+            },
+            {
+                workflow_pkey => $data->{workflow_pkey}
+            }
+        )
+    } else {
+        $data->{workflow_pkey} = $self->db->insert(
+            'workflow',
+                {
+                    $data
+                },
+                {
+                    returning => 'workflow_pkey'
+                }
+        )->hash->{workflow_pkey}
+    }
+
+    return $data->{workflow_pkey};
+}
+1;
